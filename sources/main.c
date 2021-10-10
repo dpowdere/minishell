91f0c68f9915985fe1d3a2abd04f34dca2a2a2a4
ngragas <ngragas@student.21-school.ru>
2021-10-10 19:14:47 +0300

    Test commit.

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: ngragas <ngragas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 14:22:31 by ngragas           #+#    #+#             */
-/*   Updated: 2021/10/10 14:24:05 by ngragas          ###   ########.fr       */
+/*   Updated: 2021/10/10 19:14:30 by ngragas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 int	main(void)
 {
 	printf("Hello world!");
+	printf("Testing pull request");
 	return (0);
 }
