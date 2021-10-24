f50d7373cdb3cacf7bbb360936433ba605bc79ba
ngragas <ngragas@student.21-school.ru>
2021-10-24 20:13:46 +0300

    utils.c - ft_basename() to get last component from the pathname (static storage!)

--- sources/utils.c
+++ sources/utils.c
@@ -52,6 +52,34 @@ int	ft_isspace(int c)
 			c == '\v' || c == '\f' || c == '\r');
 }
 
+char	*ft_basename(char *path)
+{
+	static char	basename_bss[MAXPATHLEN];
+	size_t		end_index;
+	size_t		start_index;
+
+	if (path == NULL || *path == '\0')
+	{
+		basename_bss[0] = '.';
+		basename_bss[1] = '\0';
+		return (basename_bss);
+	}
+	end_index = ft_strlen(path);
+	while (end_index > 1 && path[end_index - 1] == '/')
+		end_index--;
+	start_index = end_index - 1;
+	while (start_index > 0 && path[start_index - 1] != '/')
+		start_index--;
+	if (end_index - start_index >= MAXPATHLEN)
+	{
+		errno = ENAMETOOLONG;
+		return (NULL);
+	}
+	ft_memcpy(basename_bss, path + start_index, end_index - start_index);
+	basename_bss[end_index - start_index] = '\0';
+	return (basename_bss);
+}
+
 void	*error(enum e_error type, char *extra_message,
 				t_list *list_to_free, void (*free_fn)(void*))
 {
